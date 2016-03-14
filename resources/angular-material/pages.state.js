f7178a95faf66358ce7e8e3f8747e2b5f64bff45
anonymoussc <50c5ac69@opayq.com>
2016-03-15 04:55:58 +0700

    New pages.state

---
+++ resources/angular-material/pages.state.js
@@ -0,0 +1,20 @@
+/**
+ * pages.state.js
+ * Created by anonymous on 15/03/16 4:30.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('pages')
+        .config(pagesState);
+
+    pagesState.$inject = ['$stateProvider', '$urlRouterProvider', 'layoutProvider'];
+
+    /* @ngInject */
+    function pagesState($stateProvider, $urlRouterProvider, layoutProvider) {
+        //
+    }
+
+})();
